1eddedb368be420a60bea26996252852aaa15780
Adrián Prieto <72133257+Lurngi@users.noreply.github.com>
2023-04-18 16:47:10 +0200

    Add files via upload

---
+++ src/Algoritmos/Bayes.java
@@ -0,0 +1,7 @@
+package Algoritmos;
+import Jama.Matrix;
+public class Bayes {
+	private Matrix[] puntos;
+	private Matrix[] C;
+	
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,42 @@
+package Main;
+
+import java.util.ArrayList;
+
+import Algoritmos.Lloyd;
+
+public class Main {
+
+	public static void main(String[] args) {
+        ArrayList<double[]> points = new ArrayList<double[]>();
+        
+
+        points.add(arrayOf(-1.5, 2.0, 5.0, 1.2));
+        points.add(arrayOf(-2.0, -2.0, 0.0,3.4));
+        points.add(arrayOf(1.0, 1.0, 2.0,2.2));
+        points.add(arrayOf(1.5, 1.5, 1.2,0.0));
+        points.add(arrayOf(1.0, 2.0, 5.6,0.0));
+        points.add(arrayOf(1.0, -2.0, -2.0,1.2));
+        points.add(arrayOf(1.0, -3.0, -2.0,1.0));
+        points.add(arrayOf(1.0, -2.5, -4.5,1.0));
+        
+        
+        Lloyd gl = new Lloyd(points.toArray(new double[points.size()][2]));
+        double[][] results = gl.getClusterPoints(4);
+        for (double[] point : results)
+        {
+            System.out.println("Cluster " + point[0] + ", " + point[1] + ", "
+                    + point[2]);
+        }
+	}
+
+	
+    private static double[] arrayOf(double x, double y, double z, double w)
+    {
+        double[] a = new double[4];
+        a[0] = x;
+        a[1] = y;
+        a[2] = z;
+        a[3] = w;
+        return a;
+    }
+}
